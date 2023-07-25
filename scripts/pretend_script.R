17edd974d755f8b5937f825d66fd552be99a9b89
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2023-07-25 13:48:13 -0400

    Created script

---
+++ scripts/pretend_script.R
@@ -0,0 +1,11 @@
+
+data <- read.csv("data/data.csv")
+colnames(data)
+
+library(ggplot2)
+
+ggplot(data, aes(x = Title, y = Place)) +
+            geom_point(size = 5)
+
+
+ggsave("output/plot.jpg")
