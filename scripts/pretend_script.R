f566cc490361073013c07ca5555256c489c88b07
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2023-07-25 14:02:11 -0400

    Changed the axis title

--- scripts/pretend_script.R
+++ scripts/pretend_script.R
@@ -5,7 +5,8 @@ colnames(data)
 library(ggplot2)
 
 ggplot(data, aes(x = Title, y = Place)) +
-            geom_point(size = 5)
+            geom_point(size = 5) +
+            ylab("Number")
 
 
 ggsave("output/plot.jpg")
